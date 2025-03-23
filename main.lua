63f745054a2d1cd174b8c25b1de84f8c771628d1
Z1Za6yte <5yf1x.su@gmail.com>
2025-03-23 00:58:35 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,9 @@
 local function downloadFile(URL, dest)
+  if not http then
+    print("HTTP API is not enabled.")
+    return
+  end
+
   local success = shell.run("wget", URL, dest)
   if not success then
     print("Failed to download the file.")
