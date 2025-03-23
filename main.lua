190aa489b5f39004dda203af249fba94d5e4a9f5
Z1Za6yte <5yf1x.su@gmail.com>
2025-03-23 00:57:50 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,10 @@
-local function downloadFile(url,dest)
-  local response = http.get(url)
-  local success = shell.run("wget", url, dest)
+local function downloadFile(URL, dest)
+  local success = shell.run("wget", URL, dest)
+  if not success then
+    print("Failed to download the file.")
+  else
+    print("Download successful!")
+  end
 end
-downloadFile("https://github.com/5yf1xed/CC-Tweaked_SSH/raw/refs/heads/main/default/users.dat",".")
+
+downloadFile("https://github.com/5yf1xed/CC-Tweaked_SSH/raw/refs/heads/main/default/users.dat", "users.dat")
